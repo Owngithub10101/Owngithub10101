1f13e7c944713534749ea88872d6491c2f18569d
Navneet <88278303+Owngithub10101@users.noreply.github.com>
2022-02-02 13:55:24 +0530

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll=long long;
+using lld=long double;
+using ull=unsigned long long;
+#define fl(i,n) for(int i=0;i<n;i++)
+#define rl(i,m,n) for(int i = n; i >= m; i--)
+#define py cout<<"YES\n"
+#define pm cout<<"-1\n"
+#define pn cout<<"NO\n"
+
+// function...
+void machine() {
+    // Write your code here...
+    
+}
+// function end...
+
+// main...
+int main(){
+    ll n; cin>>n;
+    fl(i,n){
+        // Code is done by navneetguptacse...
+        machine();
+    }
+    return 0;
+    // end...
+}
